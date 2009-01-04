8cd8541ec8342c4264e42248696b417fd969e605
manuelmeurer <manuel.meurer@gmail.com>
2009-01-05 05:40:48 +0800

    Cleanup
    Signed-off-by: Phil Burrows <peburrows@gmail.com>

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -1,20 +1,8 @@
 class LanguageController < ApplicationController
-  # Remove this line if your controller should only be accessible to users
-  # that are logged in:
   no_login_required
   
   def set_lang
-    # setting up the :language session variable which will be referenced in the tags
-    if params[:language].downcase == 'reset'
-      session[:language] = nil
-    else
-      session[:language] = params[:language].downcase
-    end
-    
-    if !params[:from].blank?
-      redirect_to params[:from] and return
-    else
-      redirect_to '/' and return
-    end
+    session[:language] = params[:language].downcase == 'reset' ? nil : params[:language].downcase
+    redirect_to(params[:from] || '/')
   end
 end
